b7ec788e6786883773880225a97604ae50ad7a52
fangchaooo <fc_btby@hotmail.com>
2018-01-25 10:59:27 +0800

    add exception

--- Markdown2HTML/test.py
+++ Markdown2HTML/test.py
@@ -15,13 +15,15 @@ list = os.listdir(path)
 for root, dirs, files in os.walk(path):
     for filespath in files:
         if '.md' in filespath:
-            path = os.mkdir(os.path.join(root, 'MD_TO_HTML'))
-            s1 = os.path.join(root,filespath)
-            new_names = re.findall(".*/(.*)\.md",s1)
-            for new_name in new_names:
-                str = 'pandoc --css '+css_path+' ' + s1 + ' -o ' + root +'/MD_TO_HTML/'+new_name+'.html'
-                print(str)
-                os.system(str)
+            try:
+                path = os.mkdir(os.path.join(root, 'MD_TO_HTML'))
+            except BaseException:
+                s1 = os.path.join(root,filespath)
+                new_names = re.findall(".*/(.*)\.md",s1)
+                for new_name in new_names:
+                    str = 'pandoc --css '+css_path+' ' + s1 + ' -o ' + root +'/MD_TO_HTML/'+new_name+'.html'
+                    print(str)
+                    os.system(str)
 
         
 
