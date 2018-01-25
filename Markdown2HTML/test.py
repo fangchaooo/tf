e56e62eb3546402f5078d883db639927d069d757
fangchaooo <fc_btby@hotmail.com>
2018-01-25 09:54:16 +0800

    change the path

--- Markdown2HTML/test.py
+++ Markdown2HTML/test.py
@@ -3,14 +3,14 @@ import sys
 import re
 import os
 
-os.system('sudo apt-get autoremove pandoc')
-os.system('sudo apt-get install cabal-install')
-os.system('cabal update')
-os.system('cabal install pandoc')
-os.system('sudo apt-get install pandoc')
+# os.system('sudo apt-get autoremove pandoc')
+# os.system('sudo apt-get install cabal-install')
+# os.system('cabal update')
+# os.system('cabal install pandoc')
+# os.system('sudo apt-get install pandoc')
 
-path = '/home/moro/Downloads'
-css_path = '/home/moro/Downloads/UntitledFolder/1.css'
+path = input("Please input your markdown file path(using absolute path): ")
+css_path = input("Please input your markdown.css path(using absolute path): ")
 list = os.listdir(path)  
 for root, dirs, files in os.walk(path):
     for filespath in files:
@@ -20,6 +20,7 @@ for root, dirs, files in os.walk(path):
             new_names = re.findall(".*/(.*)\.md",s1)
             for new_name in new_names:
                 str = 'pandoc --css '+css_path+' ' + s1 + ' -o ' + root +'/MD_TO_HTML/'+new_name+'.html'
+                print(str)
                 os.system(str)
 
         
